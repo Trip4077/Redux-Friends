9c58fb8f62fa3fe949699845c879621ce02a02e4
Trip1701 <bj2336@email.vccs.edu>
2019-02-05 12:30:01 -0500

    getFriendReducer created

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  fetchFriends: false,
+  friendsFetched: false,
+  friends: [],
+  error: ''
+}
+
+export const getFriendReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
